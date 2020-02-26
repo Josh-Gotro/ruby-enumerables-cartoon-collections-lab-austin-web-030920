403eab8049ed73d5dab35d730ec81eadda7b26ec
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-26 16:57:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,20 +20,20 @@ def long_planeteer_calls (array)
   end
 end
 
-def find_the_cheese(a)
-  i = 0 
-  while i < a.length
-    if a.include?("cheddar") 
-      return "cheddar"
-    else if a.include?("gouda") 
-      return "gouda"
-    else if a.include?("camembert") 
-      return "camembert"
-    end end end 
-    i+=1
-  end
-  nil
-end 
+# def find_the_cheese(a)
+#   i = 0 
+#   while i < a.length
+#     if a.include?("cheddar") 
+#       return "cheddar"
+#     else if a.include?("gouda") 
+#       return "gouda"
+#     else if a.include?("camembert") 
+#       return "camembert"
+#     end end end 
+#     i+=1
+#   end
+#   nil
+# end 
 
 # cheese_types = ["cheddar", "gouda", "camembert"]
 # banana
